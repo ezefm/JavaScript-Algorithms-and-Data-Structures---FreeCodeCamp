e394ec6434646cee2cbf639635f64b67e095334c
ezefm <ezefm9926@gmail.com>
2022-12-14 10:30:28 -0300

    Commit4

--- 5.Basic Data Structures/BasicDataStructures2.js
+++ 5.Basic Data Structures/BasicDataStructures2.js
@@ -20,3 +20,171 @@ let myNestedArray = [
 ];
 
 //FreeCodeCamp (13) - JavaScript Algorithms and Data Structures - Basic Data Structures
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28
+};
+
+foods.bananas = 13;
+foods['grapes'] = 35;
+foods['strawberries'] = 27;
+
+console.log(foods);
+
+//FreeCodeCamp (14) - JavaScript Algorithms and Data Structures - Basic Data Structures
+let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
+userActivity.data.online = 45;
+console.log(userActivity);
+
+//FreeCodeCamp (15) - JavaScript Algorithms and Data Structures - Basic Data Structures
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+function checkInventory(scannedItem) {
+  return foods[scannedItem];
+}
+console.log(checkInventory("apples"));
+
+//FreeCodeCamp (16) - JavaScript Algorithms and Data Structures - Basic Data Structures
+
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+
+console.log(foods);
+
+//FreeCodeCamp (17) - JavaScript Algorithms and Data Structures - Basic Data Structures
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(userObj) {
+  return userObj.hasOwnProperty('Alan') && 
+  userObj.hasOwnProperty('Jeff') &&
+  userObj.hasOwnProperty('Sarah') &&
+  userObj.hasOwnProperty('Ryan') 
+}
+
+console.log(isEveryoneHere(users));
+
+//FreeCodeCamp (18) - JavaScript Algorithms and Data Structures - Basic Data Structures
+
+const users2 = {
+  Alan: {
+    online: false
+  },
+  Jeff: {
+    online: true
+  },
+  Sarah: {
+    online: false
+  }
+}
+
+function countOnline(usersObj) {
+  let number = 0;
+  for(let user in usersObj){
+    if(usersObj[user].online){
+      number += 1;
+    }
+  }
+  return number;
+}
+
+console.log(countOnline(users2));
+
+//FreeCodeCamp (19) - JavaScript Algorithms and Data Structures - Basic Data Structures
+
+let users3 = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  return Object.keys(obj);
+}
+
+console.log(getArrayOfUsers(users3));
+
+//FreeCodeCamp (20) - JavaScript Algorithms and Data Structures - Basic Data Structures
+
+let user4 = {
+  name: 'Kenneth',
+  age: 28,
+  data: {
+    username: 'kennethCodesAllDay',
+    joinDate: 'March 26, 2016',
+    organization: 'freeCodeCamp',
+    friends: [
+      'Sam',
+      'Kira',
+      'Tomo'
+    ],
+    location: {
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'USA'
+    }
+  }
+};
+
+function addFriend(userObj, friend) {
+  userObj.data.friends.push(friend);
+  return userObj.data.friends;
+}
+
+console.log(addFriend(user4, 'Pete'));
